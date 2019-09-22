e32a397038db9caf29eb66f7cbe19e1e2cce47a5
JoshuaRyu <43313124+JoshuaRyu@users.noreply.github.com>
2019-09-22 13:01:08 +1000

    Adding navbar with underbar highlights

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,18 +4,14 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
-
-
-
-
-        <title>Uploading file - Group PS1907</title>
+        <title>File Uploader - Group PS1907</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+        
         <!-- Styles -->
         <style>
             html, body {
@@ -68,9 +64,45 @@
             .m-b-md {
                 margin-bottom: 30px;
             }
+            
+            <!-- For navbar -->
+			.topnav {
+				overflow: hidden;
+			}
+
+			.topnav a {
+				float: left;
+				display: block;
+				color: white;
+				text-align: center;
+				padding: 14px 16px;
+				text-decoration: none;
+				font-size: 17px;
+				border-bottom: 3px solid transparent;
+			}
+
+			.topnav a:hover {
+				border-bottom: 3px solid white;
+			}
+
+			.topnav a.active {
+				border-bottom: 3px solid white;
+			}
         </style>
     </head>
     <body>
+      <header>
+	    <nav class="navbar navbar-inverse">
+		  <a class="navbar-brand" href="#home">Genisys</a>
+		  <div class="topnav nav navbar-nav navbar-right">
+			<a href="#upload">Upload</a>
+			<a href="#file">File Uploads</a>
+			<a href="#videos">Videos</a>
+		  </div>
+	    </nav>
+	  
+	  
+	</header>
         <div class="flex-center position-ref full-height">
             @if (Route::has('login'))
                 <div class="top-right links">
@@ -88,7 +120,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Uploading file - Group PS1907
+                    File Uploader - Group PS1907
                 </div>
 
                <div class="progress">
